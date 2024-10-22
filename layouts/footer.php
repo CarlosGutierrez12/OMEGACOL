d11f11ade17ad0b5f8a4aecd85b8b35a38ab379a
vamphook <martinezvictorandres@outlook.com>
2024-10-22 17:02:58 -0500

    Footer adding in progress

---
+++ layouts/footer.php
@@ -0,0 +1,26 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="inner-content">
+          <p>Copyright &copy; 2020 Sixteen Clothing Co., Ltd.
+        
+        - Design: <a rel="nofollow noopener" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="/OMEGACOL/vendor/jquery/jquery.min.js"></script>
+<script src="/OMEGACOL/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+<!-- Additional Scripts -->
+<script src="/OMEGACOL/assets/js/custom.js"></script>
+<script src="/OMEGACOL/assets/js/owl.js"></script>
+<script src="/OMEGACOL/assets/js/slick.js"></script>
+<script src="/OMEGACOL/assets/js/isotope.js"></script>
+<script src="/OMEGACOL/assets/js/accordions.js"></script>
