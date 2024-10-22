0e5dec4dbdade9a8576fc692e2e9cf07376a43ad
vamphook <martinezvictorandres@outlook.com>
2024-10-22 14:51:15 -0500

    Header added to every page

---
+++ layouts/header.php
@@ -0,0 +1,59 @@
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+  <title>Sixteen Clothing HTML Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="/OMEGACOL/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="/OMEGACOL/assets/css/fontawesome.css">
+  <link rel="stylesheet" href="/OMEGACOL/assets/css/templatemo-sixteen.css">
+  <link rel="stylesheet" href="/OMEGACOL/assets/css/owl.css">
+
+</head>
+
+<!-- ***** Preloader Start ***** -->
+<div id="preloader">
+    <div class="jumper">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>  
+<!-- ***** Preloader End ***** -->
+
+<!-- Header -->
+<header class="">
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand" href="index.php"><h2>pinturas <em>Omegacol</em></h2></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/OMEGACOL/index.php">Inicio
+              <span class="sr-only">(current)</span>
+            </a>
+          </li> 
+          <li class="nav-item">
+            <a class="nav-link" href="/OMEGACOL/products.php">Nuestros Productos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/OMEGACOL/about.php">Sobre Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/OMEGACOL/contact.php">Contáctanos</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
